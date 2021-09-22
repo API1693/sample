18e6575f64367e2b250a0abc13b5e67e343bd472
API1693 <rashmitha.r.r@apisero.com>
2021-09-22 16:08:26 +0530

    sample 1 commit

--- Sample.java
+++ Sample.java
@@ -2,5 +2,7 @@ public class Sample{
 public static void main(String[] args)
 {
 System.out.println("Fibonacci number");
+System.out.println("Fibonacci number from 1 to 100");
+System.out.println("Fibonacci number from 1 to 1000");
 }
 }
